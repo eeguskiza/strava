141de9a51f084480d028e34196ad3698b7a79c07
eeguskiza <e.eguskiza@opendeusto.es>
2024-11-07 12:43:09 +0100

    Initial Commit

---
+++ src/main/java/com/deusto/strava_case/StravaCaseApplication.java
@@ -0,0 +1,13 @@
+package com.deusto.strava_case;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StravaCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StravaCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deusto/strava_case/StravaCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusto.strava_case;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StravaCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
