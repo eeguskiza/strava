6148377007636fa6494dc66206ef7c9d4745b918
Ibonerdoiza2004 <ibon.erdoiza@opendeusto.es>
2024-12-02 08:47:59 +0100

    añadidos metodos para dao

--- src/main/java/com/deusto/strava/repository/ChallengeRepository.java
+++ src/main/java/com/deusto/strava/repository/ChallengeRepository.java
@@ -2,6 +2,10 @@ package com.deusto.strava.repository;
 
 import com.deusto.strava.entity.Challenge;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
+	Optional<Challenge>findById(Long id);
 }
--- src/main/java/com/deusto/strava/repository/TrainingSessionRepository.java
+++ src/main/java/com/deusto/strava/repository/TrainingSessionRepository.java
@@ -2,6 +2,10 @@ package com.deusto.strava.repository;
 
 import com.deusto.strava.entity.TrainingSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
 
 public interface TrainingSessionRepository extends JpaRepository<TrainingSession, Long> {
+	Optional<TrainingSession>findById(Long id);
 }
